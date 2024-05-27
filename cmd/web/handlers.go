bdb8f95ce556742d1bd3729e18452cd7342fe5fe
grahil-24 <rahilganatra@gmail.com>
2024-05-27 18:42:07 +0530

    init commit

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,20 +1,75 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
 
+// "/" route handler
 func home(w http.ResponseWriter, r *http.Request) {
+
+	wd, err := os.Getwd()
+	fmt.Println(wd)
+
 	if r.URL.Path != "/" {
+		//404 error(resource not found error)
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("hello from snippetbox"))
+	// Use the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message and use
+	// the http.Error() function to send a generic 500 Internal Server Error
+	// response to the user.
+	files := []string{"./ui/html/pages/base.gohtml", "./ui/html/partials/nav.gohtml", "./ui/html/pages/home.gohtml"}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	// We then use the Execute() method on the template set to write the
+	// template content as the response body. The last parameter to Execute()
+	// represents any dynamic data that we want to pass in, which for now we'll
+	// leave as nil.
+	//err = ts.Execute(w, nil)
+
+	// Use the ExecuteTemplate() method to write the content of the "base"
+	// template as the response body.
+	err = ts.ExecuteTemplate(w, "base", nil)
+
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
 }
 
+// "/snippet/create" route handler
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method != "POST" {
+	if r.Method != http.MethodPost {
+
+		w.Header().Set("Allow", http.MethodPost)
+		//only possible to call this method once per response
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		http.Error(w, "405 - Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create a snippet"))
+
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if id < 1 || err != nil {
 		http.NotFound(w, r)
 		return
 	}
+	fmt.Fprintf(w, "snippet #%d", id)
 
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
+	//addr variable can be used as an flag at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+	//tells go where static files are present
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	//creates new logger with 3 params
+	//- first takes the destination
+	//-
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	//strips the uri to extract the file name.
+	http.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	//creating a new server
 	mux := http.NewServeMux()
+
+	//route handler
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatalf(err)
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
 
 }
