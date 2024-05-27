5018dfea3011948edb6e03fdd83a335743806d32
grahil-24 <rahilganatra@gmail.com>
2024-05-27 18:56:31 +0530

    init commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -17,20 +17,30 @@ func main() {
 
 	//creates new logger with 3 params
 	//- first takes the destination
-	//-
+	//- second is the prefix
+	//- third is extra info like date and time
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	//strips the uri to extract the file name.
 	http.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	//creating a new server
 	mux := http.NewServeMux()
-
 	//route handler
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	err := http.ListenAndServe(*addr, mux)
+	infoLog.Print("Starting server at ", *addr)
+
+	//make the server use our custom error logger
+	srv := &http.Server{
+		ErrorLog: errorLog,
+		Handler:  mux,
+		Addr:     *addr,
+	}
+
+	err := srv.ListenAndServe()
 	log.Fatal(err)
 
 }
