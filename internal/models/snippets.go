6005ba22a0b960cd227d50e6c0793c6b702b73ef
grahil-24 <rahilganatra@gmail.com>
2024-05-29 11:53:00 +0530

    fetching data from DB

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	_ "github.com/go-sql-driver/mysql"
 	"time"
 )
 
@@ -18,10 +20,25 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(Content string, Title string, Expires int) (int, error) {
-	stmt := "Insert into snippets(content, title,  created, expires) values(?, ?, UTC_TIMESTAMP(), DATE ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))"
+	//? denotes the placeholder where we insert our data. This helps to prevent SQL injection attacks
+	stmt := "INSERT INTO snippets (title, content, created, expires) VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))"
+
+	//better to use transactions when dealing with database to preserve data consistency
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return 0, err
+	}
+
+	//if tx operations return any error the transaction is rolled back
+	defer tx.Rollback()
 
 	//exec is used for operations which do not return a row like delete and create
-	result, err := m.DB.Exec(stmt, Content, Title, Expires)
+	result, err := tx.Exec(stmt, Content, Title, Expires)
+	if err != nil {
+		return 0, err
+	}
+
+	err = tx.Commit()
 	if err != nil {
 		return 0, err
 	}
@@ -34,10 +51,45 @@ func (m *SnippetModel) Insert(Content string, Title string, Expires int) (int, e
 }
 
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
-	return nil, nil
+	stmt := `SELECT  id, title, content, created, expires FROM snippets WHERE id = ? AND expires > UTC_TIMESTAMP()`
+	s := &Snippet{}
+	row := m.DB.QueryRow(stmt, id)
+	//use Scan to get the returned values
+	//char, varchar string maps to string, int to int, BIGINT to int64 and Boolean to bool
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		//return ErrNoRows error on not finding any rows
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		}
+		return nil, err
+	}
+	//return s if all went ok
+	return s, nil
 }
 
 // get the latest 10 snippets
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets ORDER BY created DESC LIMIT 10`
+	snippets := []*Snippet{}
+	rows, err := m.DB.Query(stmt)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		s := &Snippet{}
+		err := rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, s)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
