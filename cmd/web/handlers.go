7c64d62babb8f7999a955701e9a792834ec4febd
rahil <rahilganatra@gmail.com>
2024-06-01 14:52:01 +0530

    used third party form parsers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,8 +6,21 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"snippetbox.rahilganatra.net/internal/validator"
 	"strconv"
 )
+
+// Update our snippetCreateForm struct to include struct tags which tell the
+// decoder how to map HTML form values into the different struct fields. So, for
+// example, here we're telling the decoder to store the value from the HTML form
+// input with the name "title" in the Title field. The struct tag `form:"-"`
+// tells the decoder to completely ignore a field during decoding
+type snippetCreationForm struct {
+	Title               string `form:"title"`
+	Content             string `form:"content"`
+	Expires             int    `form:"expires"`
+	validator.Validator `form:"-"`
+}
 
 // "/" route handler. home page fetches the top 10 latest snippets
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -35,23 +48,45 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	data.Form = snippetCreationForm{
+		Expires: 365,
+	}
 
-	app.render(w, http.StatusOK, "create.gohtml", app.newTemplateData(r))
+	app.render(w, http.StatusOK, "create.gohtml", data)
 }
 
 // "/snippet/create" route handler
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("inside snippetCreate")
 
-	//checking if the method is post is no longer required and can be removed
+	var form snippetCreationForm
 
-	// Create some variables holding dummy data. We'll remove these later on
-	// during the build.
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
+	// Call the Decode() method of the form decoder, passing in the current
+	// request and *a pointer* to our snippetCreateForm struct. This will
+	// essentially fill our struct with the relevant values from the HTML form.
+	// If there is a problem, we return a 400 Bad Request response to the client.
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
-	id, err := app.snippets.Insert(title, content, expires)
+	// Because the Validator type is embedded by the snippetCreateForm struct,
+	// we can call CheckField() directly on it to execute our validation checks
+	form.CheckField(validator.NotBlank(form.Title), "title", "this field cannot be empty")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "this field cannot be longer than 100 characters")
+	form.CheckField(validator.PermittedInt(form.Expires, 365, 7, 1), "expires", "This field must equal 1, 7 or 365")
+	form.CheckField(validator.NotBlank(form.Content), "content", "this field cannot be empty")
+
+	//if error exists then return them in plain text http response
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "create.gohtml", data)
+		return
+	}
+	id, err := app.snippets.Insert(form.Content, form.Title, form.Expires)
 
 	if err != nil {
 		app.serverError(w, err)
