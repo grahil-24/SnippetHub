7c6acfdfcf783f74b2a5c2bc1dd8a44dd32c1d89
rahil <rahilganatra@gmail.com>
2024-06-02 15:17:14 +0530

    server now accepts only https requests. implemented timeouts, and custom tls configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"fmt"
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form"
 	_ "github.com/go-sql-driver/mysql"
 	"html/template"
@@ -11,15 +14,17 @@ import (
 	"net/http"
 	"os"
 	"snippetbox.rahilganatra.net/internal/models"
+	"time"
 )
 
 // dependency injection so that handlers can use our custom loggers too.
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	snippets      *models.SnippetModel
-	templateCache map[string]*template.Template
-	formDecoder   *form.Decoder
+	errorLog       *log.Logger
+	infoLog        *log.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func main() {
@@ -52,12 +57,20 @@ func main() {
 	//initialize a form decode
 	formDecoder := form.NewDecoder()
 
+	//initialize a new session
+	sessionManager := scs.New()
+	//set the store for the sessions as mysql database
+	sessionManager.Store = mysqlstore.New(db)
+	//session should expire after 12 hours of creation
+	sessionManager.Lifetime = 12 * time.Hour
+
 	app := &application{
-		errorLog:      errorLog,
-		infoLog:       infoLog,
-		snippets:      &models.SnippetModel{DB: db},
-		templateCache: templateCache,
-		formDecoder:   formDecoder,
+		errorLog:       errorLog,
+		infoLog:        infoLog,
+		snippets:       &models.SnippetModel{DB: db},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
 	}
 	fmt.Println("app created\n")
 	defer func(db *sql.DB) {
@@ -69,16 +82,38 @@ func main() {
 
 	infoLog.Print("Starting server at ", *addr)
 
+	//Initialize a tls.Config struct to hold the non-default TLS settings we
+	// want the server to use. In this case the only thing that we're changing
+	// is the curve preferences value, so that only elliptic curves with
+	// assembly implementations are used.
+	tlsConfig := &tls.Config{
+		//these two are not as cpu intensive as other methods, so these will be used for better
+		//performance
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
 	//make the server use our custom error logger
 	srv := &http.Server{
-		ErrorLog: errorLog,
-		Handler:  app.routes(),
-		Addr:     *addr,
+		ErrorLog:  errorLog,
+		Handler:   app.routes(),
+		Addr:      *addr,
+		TLSConfig: tlsConfig,
+		//add idle, read and write timeouts
+		IdleTimeout: time.Minute,
+		// This means that if the request headers or body are still being read 5 seconds after the request is first accepted, then
+		//Go will close the underlying connection
+		ReadTimeout: 5 * time.Second,
+		//if data is written after writetimeout value from the time request was accepted, GO will close
+		//the connection. WriteTimeout should always be greater than ReadTimeout
+		WriteTimeout: 10 * time.Second,
 	}
 
-	err_srv := srv.ListenAndServe()
+	//ListenAndServe starts an HTTP while TLS starts an HTTPS. Before creating an HTTPS server
+	//we need to generate TLS certificates. We can do that by creating a TLS folder in our root project dir
+	//and running the command go run /usr/local/go/src/crypto/tls/generate_cert.go --rsa-bits=2048 --host=localhost
+	//in the terminal. It will generate an public and private key stored in cert.pem and key.pem files respectively
+	errSrv := srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	infoLog.Print("Server started at ", *addr)
-	log.Fatal(err_srv)
+	log.Fatal(errSrv)
 }
 
 func openDB(dsn string) (*sql.DB, error) {
