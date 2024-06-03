8fa34a95774fd495dfb56d0e52c7d72c29b7deed
rahil <rahilganatra@gmail.com>
2024-06-03 20:29:06 +0530

    added user model as app dependency

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,6 +22,7 @@ type application struct {
 	errorLog       *log.Logger
 	infoLog        *log.Logger
 	snippets       *models.SnippetModel
+	users          *models.UserModel
 	templateCache  map[string]*template.Template
 	formDecoder    *form.Decoder
 	sessionManager *scs.SessionManager
