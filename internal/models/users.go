a70008919dede765c8f5be0894871acf7ef59c17
rahil <rahilganatra@gmail.com>
2024-06-04 15:44:44 +0530

    added functionality for added users to database

--- internal/models/users.go
+++ internal/models/users.go
@@ -2,6 +2,10 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
+	"strings"
 	"time"
 )
 
@@ -19,6 +23,33 @@ type UserModel struct {
 }
 
 func (u *UserModel) Insert(name, email, password string) error {
+
+	//12 is the cost of hashing. higher the cost more secure the hash value is but also takes longer
+	//to hash the password. So choosing the right balance is important
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users(name, email, hashed_password, created) VALUES (?, ?, ?, UTC_TIMESTAMP())`
+
+	//use the Exec() method to insert the data into database
+	_, err = u.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		// If this returns an error, we use the errors.As() function to check
+		// whether the error has the type *mysql.MySQLError. If it does, the
+		// error will be assigned to the mySQLError variable. We can then check
+		// whether or not the error relates to our users_uc_email key by
+		// checking if the error code equals 1062 and the contents of the error
+		// message string. If it does, we return an ErrDuplicateEmail error.
+		var mySQLError *mysql.MySQLError
+		if errors.As(err, &mySQLError) {
+			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message, "users_uc_email") {
+				return ErrDuplicateEmail
+			}
+		}
+		return err
+	}
 	return nil
 }
 
