70a79aaf139a62bb0b7ad9a656f32b3d76a585fb
rahil <rahilganatra@gmail.com>
2024-06-08 15:41:54 +0530

    updating middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,6 +4,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 	"net/http"
+	"snippetbox.rahilganatra.net/ui"
 )
 
 func (app *application) routes() http.Handler {
@@ -26,8 +27,8 @@ func (app *application) routes() http.Handler {
 	//third party router provide routing through requets method too
 	router := httprouter.New()
 
-	//we dont want to apply sessions to serve static files
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	//we dont want to apply sessions to serve static files. Getting the static files from the EFS
+	fileServer := http.FileServer(http.FS(ui.Files))
 	//update pattern for static resource path
 	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
@@ -39,21 +40,23 @@ func (app *application) routes() http.Handler {
 	// Create a new middleware chain containing the middleware specific to our
 	// dynamic application routes. For now, this chain will only contain the
 	// LoadAndSave session middleware but we'll add more to it later.
-	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
-	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
-	//get on create returns a snippet form
-	router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
-	//post request for creating snippet handled by this
-	router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
-
 	//routers for user authentication and authorization
 	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
 	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
 	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
 	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
-	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
+	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
+
+	//these are protected routes, means these are only accessible to logged in users
+	protected := dynamic.Append(app.requireAuthentication)
+	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
+	//get on create returns a snippet form
+	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
+	//post request for creating snippet handled by this
+	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
 
 	//alice package makes middleware chaining easier
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeader)
