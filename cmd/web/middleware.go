6075d69ee36e51c1f7a7773c3c41f1df9d78b651
rahil <rahilganatra@gmail.com>
2024-06-08 15:41:26 +0530

    middleware for storing and checking authentication data from request context

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/justinas/nosurf"
 	"net/http"
 )
 
 //all custom middlewares  go here
 
-//dealing with panic
+// dealing with panic
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Create a deferred function (which will always be run in the event
@@ -45,10 +47,71 @@ func secureHeader(next http.Handler) http.Handler {
 	})
 }
 
-//middleware to log every request's info like method, ip, protocol etc.
+// middleware to log every request's info like method, ip, protocol etc.
 func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.infoLog.Printf("%s-%s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//if the request is not authenticate, means user is not logged in, we redirect the user
+		//to the home page
+		if !app.isAuthenticated(r) {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+
+		//else we add this header, which means that pages requiring authentication are not
+		//stored in cache
+		w.Header().Add("Cache-Control", "no-store")
+
+		//and call the next handler in the chain
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Create a NoSurf middleware function which uses a customized CSRF cookie with
+// the Secure, Path and HttpOnly attributes set.
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Secure:   true,
+		Path:     "/",
+	})
+
+	return csrfHandler
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Retrieve the authenticatedUserID value from the session using the
+		// GetInt() method. This will return the zero value for an int (0) if no
+		// "authenticatedUserID" value is in the session -- in which case we
+		// call the next handler in the chain as normal and return.
+		id := app.sessionManager.GetInt(r.Context(), "authenticatedUserId")
+
+		if id == 0 {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// Otherwise, we check to see if a user with that ID exists in our
+		// database.
+		exists, err := app.users.Exists(id)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		if exists {
+			ctx := context.WithValue(r.Context(), isAuthenticatedContextKey, true)
+			r = r.WithContext(ctx)
+		}
+		//call the next handler in chain
 		next.ServeHTTP(w, r)
 	})
 }
