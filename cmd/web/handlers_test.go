d7f89f1ff6fbfe4094e0b19014f338170ca82efc
grahil-24 <rahilganatra@gmail.com>
2024-06-26 16:56:38 +0530

    testing user signup form

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 	"snippetbox.rahilganatra.net/internal/assert"
 	"testing"
 )
@@ -18,6 +19,74 @@ func TestPing(t *testing.T) {
 	code, _, body := ts.get(t, "/ping")
 	assert.Equal(t, code, http.StatusOK)
 	assert.Equal(t, body, "OK")
+}
+
+func TestUserSignup(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	// Make a GET /user/signup request and then extract the CSRF token from the
+	// response body.
+	_, _, body := ts.get(t, "/user/signup")
+	validCSRFToken := extractCSRFToken(t, body)
+
+	const (
+		validName     = "Bob"
+		validPassword = "validPa$$word"
+		validEmail    = "bob@example.com"
+		formTag       = "<form action='/user/signup' method='POST' novalidate>"
+	)
+
+	tests := []struct {
+		name         string
+		userName     string
+		userEmail    string
+		userPassword string
+		csrfToken    string
+		wantCode     int
+		wantFormTag  string
+	}{
+		{
+			name:         "Valid submission",
+			userName:     validName,
+			userEmail:    validEmail,
+			userPassword: validPassword,
+			csrfToken:    validCSRFToken,
+			wantCode:     http.StatusSeeOther,
+		},
+		{
+			name:         "Invalid CSRF Token",
+			userName:     validName,
+			userEmail:    validEmail,
+			userPassword: validPassword,
+			csrfToken:    "wrongToken",
+			wantCode:     http.StatusBadRequest,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			form := url.Values{}
+			form.Add("name", tt.userName)
+			form.Add("email", tt.userEmail)
+			form.Add("password", tt.userPassword)
+			form.Add("csrf_token", tt.csrfToken)
+
+			code, _, body := ts.postForm(t, "/user/signup", form)
+
+			assert.Equal(t, code, tt.wantCode)
+
+			if tt.wantFormTag != "" {
+				assert.StringContains(t, body, tt.wantFormTag)
+			}
+		})
+	}
+
+	// Log the CSRF token value in our test output using the t.Logf() function.
+	// The t.Logf() function works in the same way as fmt.Printf(), but writes
+	// the provided message to the test output.
+	t.Logf("CSRF token is: %q", validCSRFToken)
 }
 
 func TestSnippetView(t *testing.T) {
